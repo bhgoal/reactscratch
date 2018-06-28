f623766203a01cb3d3e3e317d41bf75a23674a4a
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-06-28 10:10:52 -0500

    heroku stuff

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+// Allows us to serve files out of the client/build folder
+app.use(express.static("client/build"));
+
 app.get("/", (req, res) => {
     res.send("hi");
 });
 
-app.get("api/test", (req, res) => {
+app.get("/api/test", (req, res) => {
     res.json(true);
 });
 
-app.post("api/test", (req, res) => {
+app.post("/api/test", (req, res) => {
     console.log(req.body);
     res.json(req.body);
 });
+
+// This is a catch all if no other routes are matched
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+})
 
 app.listen(PORT, function(){
     console.log(`App listening on port ${PORT}`);
